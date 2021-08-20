080c7a84a072dbee226b55f43bab2e7c35d54d83
nikolaos-franklin <katya.nikolaos17@gmail.com>
2021-08-20 18:22:11 +0300

    added wow animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
 
+   wow = new WOW(
+   {
+   boxClass:     'wow',      // default
+   animateClass: 'animate__animated', // default
+   offset:       0,          // default
+   mobile:       true,       // default
+   live:         true        // default
+  }
+  )
+  wow.init();
+
   // для плавного скролла:
 
 $('.scrollto a').on('click', function() {
